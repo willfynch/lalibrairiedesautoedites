65de61d04f8121e4bd1e494bf4638c3ae3436327
willfynch <william.finzy@protonmail.com>
2024-10-09 17:57:34 +0200

    Add suspense boundary to navbar

--- src/components/navbar/navbar.tsx
+++ src/components/navbar/navbar.tsx
@@ -1,8 +1,9 @@
-'use client'
+"use client";
 import { NavItemModel } from "@/models/models";
 import { BOOK_TYPES, URLS_CONSTANTS } from "@/utils/constants";
 import Link from "next/link";
 import { useSearchParams } from "next/navigation";
+import { Suspense } from "react";
 import { FaBookBookmark } from "react-icons/fa6";
 
 export const navItems: NavItemModel[] = [
@@ -24,84 +25,98 @@ export const navItems: NavItemModel[] = [
   },
   {
     label: "Érotisme",
-    type: BOOK_TYPES.EROTISM
-  }
+    type: BOOK_TYPES.EROTISM,
+  },
 ];
 
-export const ACTIVE_CLASS = 'border-primary border-[1px]';
+export const ACTIVE_CLASS = "border-primary border-[1px]";
 
 function Navbar() {
-
   const searchParams = useSearchParams();
-const query = searchParams.get("type");
+  const query = searchParams.get("type");
   return (
-    <header className="sticky top-0 z-50">
-      <div className="navbar bg-base-100">
-        <div className="navbar-start">
-          <div className="dropdown">
-            <div tabIndex={0} role="button" className="btn btn-ghost lg:hidden">
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                className="h-5 w-5"
-                fill="none"
-                viewBox="0 0 24 24"
-                stroke="currentColor"
+    <Suspense>
+      <header className="sticky top-0 z-50">
+        <div className="navbar bg-base-100">
+          <div className="navbar-start">
+            <div className="dropdown">
+              <div
+                tabIndex={0}
+                role="button"
+                className="btn btn-ghost lg:hidden"
               >
-                <path
-                  strokeLinecap="round"
-                  strokeLinejoin="round"
-                  strokeWidth="2"
-                  d="M4 6h16M4 12h8m-8 6h16"
-                />
-              </svg>
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  className="h-5 w-5"
+                  fill="none"
+                  viewBox="0 0 24 24"
+                  stroke="currentColor"
+                >
+                  <path
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    strokeWidth="2"
+                    d="M4 6h16M4 12h8m-8 6h16"
+                  />
+                </svg>
+              </div>
+              <ul
+                tabIndex={0}
+                className="menu menu-sm dropdown-content bg-base-100 rounded-box z-[1] mt-3 w-52 p-2 shadow"
+              >
+                {navItems.map((item: NavItemModel, index: number) => {
+                  return (
+                    <li key={index}>
+                      <a
+                        className={item.type === query ? ACTIVE_CLASS : ""}
+                        href={`?type=${item.type}`}
+                      >
+                        {item.label}
+                      </a>
+                    </li>
+                  );
+                })}
+              </ul>
             </div>
-            <ul
-              tabIndex={0}
-              className="menu menu-sm dropdown-content bg-base-100 rounded-box z-[1] mt-3 w-52 p-2 shadow"
-            >
+            <Link className="btn btn-ghost text-md flex" href={"/?type=novel"}>
+              <FaBookBookmark />
+              La librairie des auto-édité(e)s 🏴‍☠️
+            </Link>
+          </div>
+          <div className="navbar-center hidden lg:flex">
+            <ul className="menu menu-horizontal px-1">
               {navItems.map((item: NavItemModel, index: number) => {
                 return (
-                  <li  key={index}>
-                    <a className={item.type===query ? ACTIVE_CLASS : ''} href={`?type=${item.type}`}>{item.label}</a>
+                  <li key={index}>
+                    <a
+                      className={item.type === query ? ACTIVE_CLASS : ""}
+                      href={`?type=${item.type}`}
+                    >
+                      {item.label}
+                    </a>
                   </li>
                 );
               })}
             </ul>
           </div>
-          <Link className="btn btn-ghost text-md flex" href={"/?type=novel"}>
-            <FaBookBookmark />
-            La librairie des auto-édité(e)s 🏴‍☠️
-          </Link>
-        </div>
-        <div className="navbar-center hidden lg:flex">
-          <ul className="menu menu-horizontal px-1">
-
-            {navItems.map((item: NavItemModel, index: number) => {
-              return (
-                <li  key={index}>
-                  <a className={item.type===query ? ACTIVE_CLASS : ''} href={`?type=${item.type}`}>{item.label}</a>
-                </li>
-              );
-            })}
-          </ul>
-        </div>
-        <div className="navbar-end hidden md:flex">
-          <div
-            className="tooltip tooltip-primary tooltip-bottom"
-            data-tip="Ouvrir le formulaire"
-          >
-            <a
-              role="link"
-              target="_blank"
-              href={URLS_CONSTANTS.BOOK_SUBMISSION_FORM}
-              className="btn btn-primary "
+          <div className="navbar-end hidden md:flex">
+            <div
+              className="tooltip tooltip-primary tooltip-bottom"
+              data-tip="Ouvrir le formulaire"
             >
-              Soumettre mon livre
-            </a>
+              <a
+                role="link"
+                target="_blank"
+                href={URLS_CONSTANTS.BOOK_SUBMISSION_FORM}
+                className="btn btn-primary "
+              >
+                Soumettre mon livre
+              </a>
+            </div>
           </div>
         </div>
-      </div>
-    </header>
+      </header>
+    </Suspense>
   );
 }
 
